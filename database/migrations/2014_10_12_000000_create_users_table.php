f4cbf8a4d60c1d63ac1dab35ece26341cd675446
khalil9875 <159501454+khalil9875@users.noreply.github.com>
2025-09-26 17:17:58 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -17,9 +19,21 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('is_admin')->default(0); // ✅ تم التصحيح هنا
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // ✅ إضافة مستخدم admin تلقائياً
+        DB::table('users')->insert([
+            'name' => 'khedr oniza',
+            'email' => 'khedroniza1@gmail.com',
+            'password' => Hash::make('987uykuhg7865675'), // كلمة سر افتراضية
+            'is_admin' => 1, // قيمة 1 تعني admin
+            'email_verified_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
