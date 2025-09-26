6ced2510c6089ab2f8227bf573c627326055da8c
khalil9875 <159501454+khalil9875@users.noreply.github.com>
2025-09-26 18:53:58 +0300

    Update 2025_09_24_045023_create_carts_table.php

--- database/migrations/2025_09_24_045023_create_carts_table.php
+++ database/migrations/2025_09_24_045023_create_carts_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->integer('quantity')->default(1);
-            $table->string('size')->nullable()->after('quantity');
-        $table->string('color')->nullable()->after('size');
-        $table->decimal('price', 10, 2)->after('color');
+            $table->string('size')->nullable();
+        $table->string('color')->nullable();
+        $table->decimal('price', 10, 2);
             $table->timestamps();
             
             $table->unique(['user_id', 'product_id']); // منع تكرار المنتج في السلة
